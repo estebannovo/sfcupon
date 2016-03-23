9c8e9a856f4894ae7b57876b74dc8195c2813ea0
Esteban Novo <novo.esteban@gmail.com>
2016-03-23 08:13:51 -0300

    Trabajo sobre la pagina de detalle

--- src/Cupon/OfertaBundle/Entity/OfertaRepository.php
+++ src/Cupon/OfertaBundle/Entity/OfertaRepository.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Cupon\OfertaBundle\Entity;
-
 use Doctrine\ORM\EntityRepository;
 
 class OfertaRepository extends EntityRepository
@@ -27,5 +26,43 @@ class OfertaRepository extends EntityRepository
         $consulta->setMaxResults(1);
         
         return $consulta->getSingleResult();
+    }
+    
+    public function findOferta($ciudad, $slug)
+    {
+        $em = $this->getEntityManager();
+        
+        $consulta = $em->createQuery('
+            SELECT o, c, t
+                FROM OfertaBundle:Oferta o
+                JOIN o.ciudad c JOIN o.tienda t
+            WHERE o.revisada = true
+                AND o.slug = :slug
+                AND c.slug = :ciudad');
+        
+        $consulta->setParameter('slug', $slug);
+        $consulta->setParameter('ciudad', $ciudad);
+        $consulta->setMaxResults(1);
+        
+        return $consulta->getSingleResult();
+    }
+    
+    public function findRelacionadas($ciudad)
+    {
+        $em = $this->getEntityManager();
+        
+        $consulta = $em->createQuery('
+            SELECT o, c
+                FROM OfertaBundle:Oferta o
+                JOIN o.ciudad c
+            WHERE o.revisada = true
+                AND o.fechaPublicacion <= :fecha
+                AND c.slug != :ciudad
+            ORDER BY o.fechaPublicacion DESC');
+        $consulta->setMaxResults(5);
+        $consulta->setParameter('ciudad', $ciudad);
+        $consulta->setParameter('fecha', new \DateTime('today'));
+        
+        return $consulta->getResult();
     }
 }
